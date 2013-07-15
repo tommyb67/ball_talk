e847f9689808c32a06a2bd76acf305b2ce1ddbfb
tommyb67 <tommybrennan@gmail.com>
2013-07-15 09:49:17 -0400

    Initial commit of ball talk app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallTalk::Application.config.secret_token = '1c5ee42996486c7628acb369d910d4ea0b48d412f47ef9956a3c9f956890a4040cb1c3d2b3853f9fd113c036d72b6359799e901e27e5813aa0a5baf4c44bb4b8'
