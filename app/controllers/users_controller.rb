2a1b4df0039b4b8dfcdf26e04713b2d450c91682
tommyb67 <tommybrennan@gmail.com>
2013-07-18 16:11:31 -0400

    I have a session and sessions controller.  I am going to delete the session controller if that's worng I want to be able to go back

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+
   def index
     @users = User.all
   end
 
-  def favorite_show
-    @socialplayer = Socialplayer.find(params[:id])
-
+  def add_favorite
+    #@user = User.find(params[:user_id])
+    if @current_user
+      @current_user.favorites << Socialplayer.find(params[:player_id])
+      @current_user.save
+      redirect_to 'index'
+    else
+      redirect_to 'index'
+    end
   end
 
+  def favorite_show
+    @socialplayer = Socialplayer.find(params[:id])\
+  end
 end
